67407aee288298b0dfad4b0c9bd22100152d1ffe
Buzhak <Byzhak@ya.ru>
2021-05-17 09:44:13 +0300

    compare str

---
+++ str.py
@@ -0,0 +1,13 @@
+def compare_str(str0,str1):
+    if type(str0) != str or type(str1) != str:
+        return 0
+    elif str0 != str1 and len(str0)>len(str1):
+        return 2
+    elif str0 != str1 and str1 == 'learn':
+        return 3
+    else: return 1
+
+
+str0 = '222'
+str1 = 'learn'
+print(compare_str(str0, str1))
